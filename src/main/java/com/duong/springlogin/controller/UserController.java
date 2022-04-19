16f4424dea48d7387efad9ec927b2fcb4100aadd
ThaiDuong <duongmoe174@gmail.com>
2022-04-19 17:11:03 +0700

    done

---
+++ src/main/java/com/duong/springlogin/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.duong.springlogin.controller;
+
+import com.duong.springlogin.model.Login;
+import com.duong.springlogin.model.User;
+import com.duong.springlogin.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+    @GetMapping("/home")
+    public ModelAndView hone(){
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("login", new Login());
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(Login login){
+        User user = UserService.checkLogin(login);
+        if(user == null) {
+            ModelAndView modelAndView = new ModelAndView("error");
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
+    }
+}
